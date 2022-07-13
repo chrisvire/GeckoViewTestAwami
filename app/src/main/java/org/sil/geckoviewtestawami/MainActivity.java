083a56541ea0d5103cacd3f4f2a35ab78fec00e5
Chris Hubbard <chris@thehubbards.org>
2022-07-13 13:13:57 -0400

    Add GeckoView to Main Activity and initialize

--- app/src/main/java/org/sil/geckoviewtestawami/MainActivity.java
+++ app/src/main/java/org/sil/geckoviewtestawami/MainActivity.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.mozilla.geckoview.GeckoRuntime;
+import org.mozilla.geckoview.GeckoSession;
+import org.mozilla.geckoview.GeckoView;
+
 public class MainActivity extends AppCompatActivity {
+    private static GeckoRuntime sRuntime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GeckoView view = findViewById(R.id.geckoview);
+        GeckoSession session = new GeckoSession();
+
+        // Workaround for Bug 1758212
+        session.setContentDelegate(new GeckoSession.ContentDelegate() {});
+
+        if (sRuntime == null) {
+            // GeckoRuntime can only be initialized once per process
+            sRuntime = GeckoRuntime.create(this);
+        }
+
+        session.open(sRuntime);
+        view.setSession(session);
+        session.loadUri("about:buildconfig");
+
     }
 }
